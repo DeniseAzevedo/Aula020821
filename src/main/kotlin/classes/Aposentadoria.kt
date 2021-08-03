a904e5642338bd4d9f8105e714ecceb9c140fd01
Denise Azevedo <denise.azevedopereira@gmail.com>
2021-08-02 22:32:29 -0300

    resolução exercício numero 1 da aula 020821 - Classes

---
+++ src/main/kotlin/classes/Aposentadoria.kt
@@ -0,0 +1,23 @@
+package classes
+
+class Aposentadoria (var idade: Int, var sexo: String, var anosContribuicao: Int) {
+
+    fun aposentar() {
+        //mulheres
+        if(sexo == "feminino") {
+            if ((idade >= 60) && (anosContribuicao >= 30)) {
+                println("verdadeiro, pode se aposentar")
+            } else {
+                println("falso, não pode se aposentar")
+            }
+        } else {
+            if (sexo == "masculino")
+                if (idade >= 65 && anosContribuicao >= 30) {
+                    println("verdadeiro, pode se aposentar")
+                } else {
+                    println("falso, não pode se aposentar")
+                }
+           }
+
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+import classes.Aposentadoria
+
+fun main() {
+    var aposentadoria = Aposentadoria(65, "masculino", 24)
+    println(aposentadoria.aposentar())
+}
